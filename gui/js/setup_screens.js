599a0b341374ee2f0c75ba0be90deae69f3ef735
Minu <minuapple@gmail.com>
2017-06-22 16:57:59 +0530

    added support for displaying all buses and fetches all depos for add bus formm

--- gui/js/setup_screens.js
+++ gui/js/setup_screens.js
@@ -1,5 +1,31 @@
 $( document ).ready(function() {
 
+	//getting all buses initially to display in the table
+	$.ajax({
+		url: 'utils/get_all_buses.php',
+		type: 'POST',
+		success: function(result) {
+			var buses = $.parseJSON(result);
+			// console.log(buses);
+			for (var i = 0; i < buses.length; i++) {
+				$('.bus_table tbody').append('<tr><td colspan="1"><input type="radio" name="groups"></td><td colspan="2">'+(i+1)+'</td><td colspan="4">'+buses[i].RegistrationNo+'</td><td colspan="4">'+buses[i].Name+'</td><td colspan="4">'+buses[i].PhoneNumberOfSCU+'</td></tr>')
+			}
+		}
+	});
+
+	$.ajax({
+		url: 'utils/get_all_depos.php',
+		type: 'POST',
+		success: function(result) {
+			var depos = $.parseJSON(result);
+			console.log(depos);
+			for (var i = 0; i < depos.length; i++) {
+				$('.depo_name_select').append('<option value='+ depos[i].ID +'>'+ depos[i].Name +'</option>')
+			}
+		}
+	});
+
+	//adding a new bus
 	$('.add_bus').click(function(){
 		var bus_registration_number = $('.bus_registration_number').val();
 		var depo_id = $('.depo_name_select').val();
