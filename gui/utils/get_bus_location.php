90d13c82f7ccb7b5280d47d7a4e963f35e9c99f5
Minu <minuapple@gmail.com>
2017-06-23 13:33:04 +0530

    added bus location on map and function to update it every 5 seconds

--- gui/utils/get_bus_location.php
+++ gui/utils/get_bus_location.php
@@ -8,7 +8,7 @@ try {
 	$handle=$link->prepare("SELECT * FROM `location_log` WHERE `BusRegNo`=:bus_reg_no ORDER BY TimeStamp DESC Limit 1");
 	$handle->bindParam(':bus_reg_no', $bus_reg_no); 
 	$handle->execute();
-	$result = $handle->fetchall(PDO::FETCH_ASSOC);
+	$result = $handle->fetch(PDO::FETCH_ASSOC);
 	return json_encode($result);
     }
 
